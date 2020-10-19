435d01d3cf830e3fd499f24586371790d5dd2690
devferx <ferkki@hotmail.com>
2020-10-19 19:16:58 -0400

    animaciones agregadas al boton de buscar

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableWithoutFeedback,
+  Animated,
 } from 'react-native';
 import {Picker} from '@react-native-community/picker';
 
 const Formulario = () => {
+  const [animacionBoton] = useState(new Animated.Value(1));
+
+  const animacionEntrada = () => {
+    Animated.spring(animacionBoton, {
+      toValue: 0.75,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const animacionSalida = () => {
+    Animated.spring(animacionBoton, {
+      toValue: 1,
+      friction: 4,
+      tension: 30,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const estiloAnimacion = {
+    transform: [{scale: animacionBoton}],
+  };
+
   return (
     <View style={styles.formulario}>
       <View>
@@ -31,10 +54,12 @@ const Formulario = () => {
         </Picker>
       </View>
 
-      <TouchableWithoutFeedback>
-        <View style={styles.btnBuscar}>
+      <TouchableWithoutFeedback
+        onPressIn={() => animacionEntrada()}
+        onPressOut={() => animacionSalida()}>
+        <Animated.View style={[styles.btnBuscar, estiloAnimacion]}>
           <Text style={styles.textoBuscar}>Buscar Clima</Text>
-        </View>
+        </Animated.View>
       </TouchableWithoutFeedback>
     </View>
   );
