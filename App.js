1566c8e48581c2bb31906934428f8a12acd2caa9
devferx <ferkki@hotmail.com>
2020-10-19 20:55:47 -0400

    agregado el  cambio de color en el fondo

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   Alert,
   TouchableWithoutFeedback,
 } from 'react-native';
+import {act} from 'react-test-renderer';
 import Clima from './components/Clima';
 import Formulario from './components/Formulario';
 
@@ -13,6 +14,7 @@ const App = () => {
   const [busqueda, setBusqueda] = useState({ciudad: '', pais: ''});
   const [consultar, setConsultar] = useState(false);
   const [resultado, setResultado] = useState({});
+  const [bgColor, setBgColor] = useState('rgb(71,149,219)');
 
   const {ciudad, pais} = busqueda;
 
@@ -27,6 +29,18 @@ const App = () => {
           const resultadoApi = await respuesta.json();
           setResultado(resultadoApi);
           setConsultar(false);
+
+          // Modifica los colores de fondo basados en la temperatura
+          const kelvin = 273.15;
+          const {main} = resultado;
+          const actual = main.temp - kelvin;
+          if (actual < 10) {
+            setBgColor('rgb(105,108,149)');
+          } else if (actual >= 10 && actual < 25) {
+            setBgColor('rgb(71,149,219)');
+          } else {
+            setBgColor('rgb(178,28,61)');
+          }
         } catch (error) {
           mostrarAlerta();
         }
@@ -34,7 +48,7 @@ const App = () => {
     };
 
     consultarClima();
-  }, [consultar, ciudad, pais]);
+  }, [consultar, ciudad, pais, resultado]);
 
   const mostrarAlerta = () => {
     Alert.alert('Error', 'No hay resultado intenta con otra ciudad o pais', [
@@ -46,9 +60,13 @@ const App = () => {
     Keyboard.dismiss();
   };
 
+  const bgColorApp = {
+    backgroundColor: bgColor,
+  };
+
   return (
     <TouchableWithoutFeedback onPress={ocultarTeclado}>
-      <View style={styles.app}>
+      <View style={[styles.app, bgColorApp]}>
         <View style={styles.contenido}>
           <Clima resultado={resultado} />
           <Formulario
@@ -67,7 +85,6 @@ export default App;
 const styles = StyleSheet.create({
   app: {
     flex: 1,
-    backgroundColor: 'rgb(71,149,219)',
     justifyContent: 'center',
   },
   contenido: {
